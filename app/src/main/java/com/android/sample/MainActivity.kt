334ad4954a82a0ef8eddbea4150e4b4aa34af22c
Kenza_Hassayoune <kenza.hassayoune@epfl.ch>
2025-10-12 12:02:40 +0200

    Testing CI/Sonar

--- app/src/main/java/com/android/sample/MainActivity.kt
+++ app/src/main/java/com/android/sample/MainActivity.kt
@@ -3,6 +3,9 @@ package com.android.sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -17,15 +20,14 @@ class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      /* SampleAppTheme {
+      SampleAppTheme {
         // A surface container using the 'background' color from the theme
         Surface(
             modifier = Modifier.fillMaxSize().semantics { testTag = C.Tag.main_screen_container },
             color = MaterialTheme.colorScheme.background) {
               Greeting("Android")
             }
-      }*/
-      SampleAppTheme { PlannerScreen() }
+      }
     }
   }
 }
