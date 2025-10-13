3cfa2065128ee75f3bf607d8a860f152f3de3edc
DellaSantaF <71725757+DellaSantaF@users.noreply.github.com>
2025-10-13 14:02:01 +0200

    test(pomodoro): add tests for timer logic and skip button debounce to increase test coverage

--- app/src/main/java/com/android/sample/ui/pomodoro/PomodoroTimerScreen.kt
+++ app/src/main/java/com/android/sample/ui/pomodoro/PomodoroTimerScreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -153,10 +152,4 @@ fun PomodoroButton(
       modifier = modifier) {
         Text(text)
       }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PomodoroScreenPreview() {
-  PomodoroScreen()
 }
