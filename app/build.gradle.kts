2a2028920d714203fc3d9ede17864a22395bff7b
Kenza_Hassayoune <kenza.hassayoune@epfl.ch>
2025-10-13 19:53:38 +0200

    Testing CI/Sonar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,6 +104,7 @@ sonar {
         property("sonar.androidLint.reportPaths", "${project.layout.buildDirectory.get()}/reports/lint-results-debug.xml")
         // Paths to JaCoCo XML coverage report files.
         property("sonar.coverage.jacoco.xmlReportPaths", "${project.layout.buildDirectory.get()}/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
+        property("sonar.tests", "src/test/java,src/androidTest/java")
     }
 }
 
