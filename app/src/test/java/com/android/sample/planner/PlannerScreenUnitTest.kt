4c975005f273a1b6f3eeac3c759b80ccb40a1710
Kenza_Hassayoune <kenza.hassayoune@epfl.ch>
2025-10-15 15:43:43 +0200

    Pass the CI/Sonar

--- app/src/test/java/com/android/sample/planner/PlannerScreenUnitTest.kt
+++ app/src/test/java/com/android/sample/planner/PlannerScreenUnitTest.kt
@@ -1,18 +1,15 @@
 package com.android.sample.planner
 
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.test.junit4.createComposeRule
 import com.android.sample.ui.planner.PlannerScreen
 import org.junit.Rule
 import org.junit.Test
 
 class PlannerScreenUnitTest {
-    @get:Rule val composeTestRule = createComposeRule()
+  @get:Rule val composeTestRule = createComposeRule()
 
-    @Test
-    fun composePlannerScreen_doesNotCrash() {
-        composeTestRule.setContent {
-            PlannerScreen()
-        }
-    }
+  @Test
+  fun composePlannerScreen_doesNotCrash() {
+    composeTestRule.setContent { PlannerScreen() }
+  }
 }
