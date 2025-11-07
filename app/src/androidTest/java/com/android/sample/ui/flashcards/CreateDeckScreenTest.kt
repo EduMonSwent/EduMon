cd8bd8e92d34c430e6814698962853251fe171a6
Ahmed Azaiez <ahmedazaiez@MacBook-Pro-de-Ahmed.local>
2025-11-07 03:07:56 +0100

    added an import to createdeckscreentest that was failing the ci

--- app/src/androidTest/java/com/android/sample/ui/flashcards/CreateDeckScreenTest.kt
+++ app/src/androidTest/java/com/android/sample/ui/flashcards/CreateDeckScreenTest.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.android.sample.repos_providors.FakeRepositories
 import com.android.sample.ui.flashcards.data.FlashcardsRepositoryProvider
 import com.android.sample.ui.theme.EduMonTheme
 import java.util.concurrent.atomic.AtomicBoolean
