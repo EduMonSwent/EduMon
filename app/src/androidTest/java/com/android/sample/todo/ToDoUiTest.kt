0ffb070a460f2d396eddfadcb330286e6e308ec4
alaaae <alae.messaoudi@epfl.ch>
2025-11-27 11:50:24 +0100

    fix tests

--- app/src/androidTest/java/com/android/sample/todo/ToDoUiTest.kt
+++ app/src/androidTest/java/com/android/sample/todo/ToDoUiTest.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.performTextClearance
 import androidx.compose.ui.test.performTextInput
 import com.android.sample.data.Priority
 import com.android.sample.data.Status
