11123a1d79141176b36515b43a24019ec7262093
Ahmed Azaiez <ahmedazaiez@MacBook-Pro-de-Ahmed.local>
2025-12-11 13:17:35 +0100

    Test : Add new tests to increase line coverage

--- app/src/main/java/com/android/sample/ui/profile/StatGainEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.android.sample.ui.profile
-
-sealed class StatGainEvent {
-  data class PointsGained(val amount: Int) : StatGainEvent()
-
-  data class CoinsGained(val amount: Int) : StatGainEvent()
-}
