ec956d8c718e266876f610dff0fbc38cf6d04b1a
alaaae <alae.messaoudi@epfl.ch>
2025-12-17 22:35:17 +0100

    removed wrong test

--- app/src/androidTest/java/com/android/sample/NavigationAdditionnalTests.kt
+++ app/src/androidTest/java/com/android/sample/NavigationAdditionnalTests.kt
@@ -1,18 +1,14 @@
 package com.android.sample
 
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.performTouchInput
-import androidx.compose.ui.test.swipe
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.android.sample.feature.homeScreen.AppDestination
 import com.android.sample.feature.homeScreen.HomeTestTags
 import com.android.sample.repos_providors.AppRepositories
 import com.android.sample.repos_providors.FakeRepositoriesProvider
-import com.android.sample.ui.profile.ProfileScreenTestTags
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -66,69 +62,6 @@ class NavigationAdditionnalTests {
             composeTestRule
                 .onNodeWithTag(NavigationTestTags.TOP_BAR_TITLE)
                 .assertTextEquals("Study")
-          }
-          .isSuccess
-    }
-
-    composeTestRule.onNodeWithTag(NavigationTestTags.TOP_BAR_TITLE).assertExists()
-  }
-
-  @Test
-  fun profile_screen_onOpenFocusMode_lambda_executes() {
-    composeTestRule.setContent { EduMonNavHost(startDestination = AppDestination.Home.route) }
-    composeTestRule.waitForIdle()
-    waitForHomeScreen()
-
-    // Home -> Profile
-    composeTestRule.onNodeWithTag(HomeTestTags.MENU_BUTTON).performClick()
-    composeTestRule.waitForIdle()
-    composeTestRule
-        .onNodeWithTag(HomeTestTags.drawerTag(AppDestination.Profile.route))
-        .performClick()
-    composeTestRule.waitForIdle()
-
-    // Confirm Profile
-    composeTestRule.waitUntil(timeoutMillis = 10_000) {
-      runCatching {
-            composeTestRule
-                .onNodeWithTag(NavigationTestTags.TOP_BAR_TITLE)
-                .assertTextEquals("Profile")
-          }
-          .isSuccess
-    }
-
-    // Wait for Profile root
-    composeTestRule.waitUntil(timeoutMillis = 10_000) {
-      runCatching {
-            composeTestRule.onNodeWithTag(ProfileScreenTestTags.PROFILE_SCREEN).assertExists()
-          }
-          .isSuccess
-    }
-
-    // Scroll down a bit (keep your original robust swipe, in case performScrollTo isn't supported)
-    repeat(3) {
-      composeTestRule.onNodeWithTag(ProfileScreenTestTags.PROFILE_SCREEN).performTouchInput {
-        val size = this@performTouchInput.visibleSize
-        val startY = (size.height * 0.80f)
-        val endY = (size.height * 0.20f)
-        val centerX = (size.width / 2.0f)
-        swipe(start = Offset(centerX, startY), end = Offset(centerX, endY), durationMillis = 500)
-      }
-      composeTestRule.waitForIdle()
-    }
-
-    // Toggle focus mode switch
-    composeTestRule
-        .onNodeWithTag(ProfileScreenTestTags.SWITCH_FOCUS_MODE, useUnmergedTree = true)
-        .assertExists()
-        .performClick()
-
-    // Wait for Focus Mode destination
-    composeTestRule.waitUntil(timeoutMillis = 10_000) {
-      runCatching {
-            composeTestRule
-                .onNodeWithTag(NavigationTestTags.TOP_BAR_TITLE)
-                .assertTextEquals("Focus Mode")
           }
           .isSuccess
     }
